27f8019f469fe1a8f07cda7dfc856dc400ea02df
Henrik Jacobsen <henrik259@hotmail.com>
2018-10-16 08:40:20 +0200

    Add project files.

---
+++ Brute Force/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brute_Force
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
